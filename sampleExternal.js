2b7ffbe5eb481d6e986af1c3444edaf97eea53c9
anandcute <d.anandkumar2002@gmail.com>
2024-05-11 15:21:01 +0530

    commit

---
+++ sampleExternal.js
@@ -0,0 +1,17 @@
+
+        function changeName(){
+            document.getElementById('name').innerHTML="1.BEN TEN ULTIMATE ALIEN"
+        }
+        function changeName1(){
+            document.getElementById('name1').innerHTML="2.TOM AND  JERRY SHOWS"
+        }
+        function changeName2(){
+            document.getElementById('name2').innerHTML="3.BLACK SUIT SPIDERMAN"
+        }
+        function changeName3(){
+            document.getElementById('name3').innerHTML="4.POWER RANGERS WILD FORCE"
+        }
+        function changeName4(){
+            document.getElementById('name4').innerHTML="5.TEEN TITANS."
+        }
+   
